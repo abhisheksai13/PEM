318b9d5ddbd8c652efd78931b775ef5e2e68d55c
Sai Abhishek <abhisheksai13@gmail.com>
2023-07-12 22:03:44 -0400

    adding category file to git

---
+++ src/module/first/Category.java
@@ -0,0 +1,35 @@
+package module.first;
+
+public class Category {
+
+    private Long  categoryId= System.currentTimeMillis();
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Category(Long categoryId, String name) {
+        this.categoryId = categoryId;
+        this.name = name;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
