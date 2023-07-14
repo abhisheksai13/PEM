a5e44ae64f027c13a790dfee4eddeb1e360cd694
Sai Abhishek <abhisheksai13@gmail.com>
2023-07-13 20:38:07 -0400

    adding the skeleton for menu

--- src/module/first/PEMService.java
+++ src/module/first/PEMService.java
@@ -1,5 +1,6 @@
 package module.first;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class PEMService {
@@ -12,10 +13,44 @@ public class PEMService {
             switch (choice){
                 case 1:
                     //TODO for adding categories
-                    System.out.println("Enter categories");
+                    onAddCategory();
+                    System.out.println("Enter categories.......");
+                    pressAnyKeyToContinue();
+                    break;
+
+                case 2:
+                    //TODO for category List
+                    onCategoryList();
+                    pressAnyKeyToContinue();
+                    break;
+                case 3:
+                    //TODO for expense entry
+                    onExpenseEntry();
+                    pressAnyKeyToContinue();
+                    break;
+                case 4:
+                    //TODO for expense entry
+                    onExpenseList();
+                    pressAnyKeyToContinue();
+                    break;
+
+                case 5:
+                    //TODO for yearly expense List
+                    onMonthlyExpenseEntry();
+                    pressAnyKeyToContinue();
+                    break;
+                case 6:
+                    //TODO for expense entry
+                    onYearlyExpenseList();
+                    pressAnyKeyToContinue();
+                    break;
+                case 7:
+                    //TODO for expense entry
+                    onCategorizedExpenseEntry();
+                    pressAnyKeyToContinue();
                     break;
                 case 0:
-                    System.exit(0);
+                    exit();
                     break;
             }
         }
@@ -38,4 +73,50 @@ public class PEMService {
         choice=in.nextInt();
 
     }
+
+    public void pressAnyKeyToContinue(){
+
+        try {
+            System.out.println("Press any key to continue........");
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void onAddCategory() {
+        System.out.println("adding categories");
+        //TODO
+    }
+    private void onExpenseEntry() {
+        System.out.println("expense Entry");
+        //TODO
+    }
+
+    public void onExpenseList() {
+    }
+
+    private void onCategoryList() {
+        System.out.println("category List");
+        //TODO
+    }
+    private void onMonthlyExpenseEntry() {
+        System.out.println("monthly Expense entry");
+        //todo
+    }
+
+    public void onYearlyExpenseList() {
+    }
+
+    public void onCategorizedExpenseEntry() {
+        //Todo
+        System.out.println("categorized Expense entry");
+    }
+
+    public void exit(){
+        System.exit(0);
+    }
+
+
+
 }
